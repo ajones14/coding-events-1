dbcf36e9cf1ad07d7fdfe703076da3be5825e57d
Abigail Wiggins <rebtaylor14@gmail.com>
2020-01-27 01:38:08 -0600

    Chapter 18 code along finished

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -3,16 +3,22 @@ package org.launchcode.codingevents.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
-public class EventCategory extends AbstractEntity {
+public class EventCategory<Events> extends AbstractEntity {
 
     @NotBlank(message = "Name is required.")
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private List<Event> events = new ArrayList<>();
 
     public EventCategory(@Size(min=3, message="Name must be at least 3 characters long") String name) {
         this.name = name;
@@ -26,6 +32,10 @@ public class EventCategory extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
