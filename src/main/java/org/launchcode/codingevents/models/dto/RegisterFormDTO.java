9e366a19c761cd089e8a536f2100894fda93c00a
Abigail Wiggins <rebtaylor14@gmail.com>
2020-02-03 01:58:20 -0600

    Chapter 19 lesson completed

---
+++ src/main/java/org/launchcode/codingevents/models/dto/RegisterFormDTO.java
@@ -0,0 +1,17 @@
+package org.launchcode.codingevents.models.dto;
+
+import com.mysql.cj.log.Log;
+
+public class RegisterFormDTO extends LoginFormDTO {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+}
